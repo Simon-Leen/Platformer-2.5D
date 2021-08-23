c84cb82d68eb0f39fc4cd680b4dba56194577b54
= <=>
2021-08-23 07:28:01 +0100

    Finished puzzles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,10 @@ public class Player : MonoBehaviour
     [SerializeField] private int _coins;
     private UIManager _uIManager;
     private int _lives = 3;
+    private Vector3 _direction, _velocity;
+    private bool _canWallJump = false;
+    private Vector3 _wallSurfaceNormal;
+    private float _pushPower = 2.0f;
 
     void Start()
     {
@@ -30,12 +34,14 @@ public class Player : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(horizontalInput, 0, 0);
-        Vector3 velocity = direction * _speed;
         if(_controller.enabled == true)
         {
             if (_controller.isGrounded)
             {
+                _canWallJump = false;
+                _direction = new Vector3(horizontalInput, 0, 0);
+                _velocity = _direction * _speed;
+
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     _yVelocity = _jump;
@@ -44,15 +50,46 @@ public class Player : MonoBehaviour
             }
             else
             {
-                if (Input.GetKeyDown(KeyCode.Space) && _canDoubleJump)
+                if (Input.GetKeyDown(KeyCode.Space) && _canWallJump == false)
                 {
-                    _yVelocity += _jump;
-                    _canDoubleJump = false;
+                    if(_canDoubleJump == true)
+                    {
+                        _yVelocity += _jump;
+                        _canDoubleJump = false;
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.Space) && _canWallJump == true)
+                {
+                    if(_canDoubleJump == true)
+                    {
+                        _canDoubleJump = false;
+                    }
+                    _yVelocity = _jump;
+                    _velocity = _wallSurfaceNormal * _speed;
                 }
                 _yVelocity -= _gravity;
             }
-            velocity.y = _yVelocity;
-            _controller.Move(velocity * Time.deltaTime);
+            _velocity.y = _yVelocity;
+            _controller.Move(_velocity * Time.deltaTime);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.transform.tag == "Moveable")
+        {
+            Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0, 0);
+                rb.velocity = pushDirection * _pushPower;
+            }
+        }
+        if(_controller.isGrounded == false && hit.transform.tag == "Wall")
+        {
+            //Debug.DrawRay(hit.point, hit.normal, Color.blue);
+            _wallSurfaceNormal = hit.normal;
+            _canWallJump = true;
         }
     }
 
@@ -74,5 +111,10 @@ public class Player : MonoBehaviour
         {
             _uIManager.UpdateLivesDisplay(_lives);
         }
+    }
+
+    public int Coins()
+    {
+        return _coins;
     }
 }
